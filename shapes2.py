20d9ecaef0667c6b53ab01f8855f5430bcb06511
zygradeA <81732838+zygradeA@users.noreply.github.com>
2021-04-05 19:47:43 +0800

    Update shapes2.py
    Can now make iscoeles triangle

--- shapes2.py
+++ shapes2.py
@@ -1,24 +1,44 @@
 import turtle as pen
+import math as m
+turt = pen
+
+
 
 def shapecreate(numberofside,sidelength,angle):
     for i in range(numberofside):
         pen.forward(sidelength)
-        pen.left(angle)
-    def triangle(base,height):
-        angle = m.degrees(m.atan(2*height/base))
-        side = m.sqrt(height**2+((base**2)/4))
+        pen.right(angle)
+
+
+def triangle(base,height,x,y,edge_thickness = None,edge_colour = None,fill_colour = None):
+    pen.penup()
+    turt.goto(x,y)
+    turt.setheading(0)
+    turt.pen(pensize=edge_thickness, pencolor=edge_colour, fillcolor=fill_colour)
+    turt.pendown()
+    turt.begin_fill()
+
+    angle = m.degrees(m.atan(2*height/base))
+    side = m.sqrt(height**2+((base**2)/4))
     #thank you so much to the relief math and science teacher i had in science for teaching me how to do these equations
 
-        pen.forward(base)
-        pen.setheading(180)
-        pen.right(angle)
-        pen.forward(side)
-        pen.penup()
-        pen.pendown()
-        pen.setheading(0)
-        pen.left(angle)
-        pen.forward(side)
-    return shapecreate
+    turt.forward(base)
+    turt.setheading(180)
+    turt.right(angle)
+    turt.forward(side)
+    turt.penup()
+    turt.goto(x,y)
+    turt.pendown()
+    turt.setheading(0)
+    turt.left(angle)
+    turt.forward(side)
+
+    turt.end_fill()
 
 
-
+def rectangle(sidelength,sidelengthmultiplier):
+    for i in range(2):
+        pen.right(90)
+        pen.forward(sidelength*sidelengthmultiplier)
+        pen.right(90)
+        pen.forward(sidelength)
